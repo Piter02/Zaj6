41cb031b33a8a11830b6f7bc7fc5a6d8e6d0464d
Piotr <piotrek8pg@gmail.com>
2024-05-08 10:57:41 +0200

    dod7

---
+++ utils.py
@@ -0,0 +1,22 @@
+# przykladowe funkcje utils.py
+"""Moduł zawierający funkcje matematyczne"""
+
+
+def add(a: int, b: int) -> int:
+    """Funkcja dodaje dwie liczby i zwraca wynik"""
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """Funkcja odejmuje dwie liczby i zwraca wynik"""
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    """Funkcja mnoży dwie liczby i zwraca wynik"""
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    """Funkcja dzieli dwie liczby i zwraca wynik"""
+    return a / b
